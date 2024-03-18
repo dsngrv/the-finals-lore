bfe97737a8f9f7e33bc4a5da213ab4fff2c0e3f8
Дмитрий Снигирев <dmitry@iMac-Dmitrij.local>
2024-03-18 16:27:39 +0400

    first commit

---
+++ THE FINALS LORE/THE_FINALS_LOREApp.swift
@@ -0,0 +1,17 @@
+//
+//  THE_FINALS_LOREApp.swift
+//  THE FINALS LORE
+//
+//  Created by Дмитрий Снигирев on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct THE_FINALS_LOREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
